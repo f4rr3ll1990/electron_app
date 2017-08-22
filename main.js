177b31374757d15129119f9f13282c311b6311c0
F4rr3LL <shadow_2pac@mail.ru>
2017-08-22 17:55:40 +0300

    кастом мею

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const remote = require('electron').remote
 // получаем доступ к экспортированным методам из index.js
 const main = remote.require('./index.js')
+// получаем доступ к меню окна
+const Menu = remote.require('electron').Menu
+// создаем новое меню
+const menu = Menu.buildFromTemplate([
+    {
+        label: 'Меню',
+        submenu: [
+            {
+                label: 'тест',
+                click: function(){
+                    main.openWindow()
+                }
+            }
+        ]
+    }
+])
+// инициализируем меню
+Menu.setApplicationMenu(menu)
 
 
 let alertButton = document.getElementById('alert')
